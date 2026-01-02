5b56ed9d604ac8de874b2e9a7699acf0c437f111
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2026-01-02 08:19:06 +0530

    shell

--- 05-date.sh
+++ 05-date.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 Raj=$(date +"%Y-%m-%d %H:%M:%S")
 echo "Today date is $Raj "
+Start_Time=$(date +%s)
+sleep 10
+End_Time=$(date +%s)
+
+Log_Time= $((End_Time-Start_Time))
+echo "Toal time is :: $Log_Time"
