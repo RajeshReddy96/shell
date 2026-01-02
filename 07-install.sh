754e74842058bb6741bf06e0910a4a475663ac68
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2026-01-02 09:18:50 +0530

    shell

--- 07-install.sh
+++ 07-install.sh
@@ -4,7 +4,7 @@ if [ $USERID -ne 0 ]; then
 echo "ERROR: PLease run with root access"
 exit 1
 fi
-dnf install mysql -y
+dnf install mysqql -y
 if [ $? -ne 0 ]; then
 echo "ERROR: mysql installing failed"
 exit 1
