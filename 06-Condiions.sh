607835bd051901976828d7af6fe3b6908b2cf917
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2026-01-02 08:40:21 +0530

    shell

--- 06-Condiions.sh
+++ 06-Condiions.sh
@@ -1,8 +1,19 @@
-#!/bin/bash
-echo "Enter Your Number :: $NUM"
-read NUM
-echo "Entered NUmber is :: $NUM"
+# #!/bin/bash
+# echo "Enter Your Number :: $NUM"
+# read NUM
+# echo "Entered NUmber is :: $NUM"
 
+# if [ $NUM -lt 111 ]; then
+# echo "Given Number $NUM is lesss then 111"
+# elif [ $NUM -eq 111 ]; then
+# echo "Given Number $NUM equal"
+# else
+# echo "Given Number $NUM is greather then 111"
+# fi
+
+#!/bin/bash
+NUM=$1
+echo "Entered NUmber is :: $NUM"
 if [ $NUM -lt 111 ]; then
 echo "Given Number $NUM is lesss then 111"
 elif [ $NUM -eq 111 ]; then
@@ -10,4 +21,3 @@ echo "Given Number $NUM equal"
 else
 echo "Given Number $NUM is greather then 111"
 fi
-
