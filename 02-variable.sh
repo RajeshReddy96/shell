8b121275e08e3a4655c148b3f4d41fdd342503fc
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2026-01-02 08:04:45 +0530

    shell

--- 02-variable.sh
+++ 02-variable.sh
@@ -2,5 +2,5 @@
 person1=$1
 person2=$2
 
-echo "$person1 :: hey $perosn2 where are you man?"
+echo "$person1 :: hey $person2 where are you man?"
 echo  "$person2 :: i am in Alaska rey $person1"
---
+++ 03-read.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Enter Pin for your ATM" 
+read pin
+echo "your entered number is $pin"
